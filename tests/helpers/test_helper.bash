dba52b02ab3011303f36abb6bd6bb05464f52fbd
holgern <holgern@users.noreply.github.com>
2026-02-24 11:12:41 +0100

    test: add tests

---
+++ tests/helpers/test_helper.bash
@@ -0,0 +1,10 @@
+setup() {
+    TEST_TEMP="$(mktemp -d)"
+    FIXTURES_DIR="$(dirname "$BATS_TEST_DIRNAME")/tests/fixtures"
+    export TEST_TEMP
+    export FIXTURES_DIR
+}
+
+teardown() {
+    rm -rf "$TEST_TEMP"
+}
